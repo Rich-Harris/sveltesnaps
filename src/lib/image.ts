34779f1674dcbd9fe4215a064e1f69f6b161ef26
Rich Harris <git@rich-harris.dev>
2023-05-03 17:46:54 -0400

    man srcset is confusing

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -5,7 +5,7 @@ export function optimize(src: string, widths = [640, 960, 1280], quality = 75) {
 
 	return widths
 		.slice()
-		.sort((a, b) => b - a)
+		.sort((a, b) => a - b)
 		.map((width, i) => {
 			const url = `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
 			const descriptor = i < widths.length - 1 ? ` ${width}w` : '';
