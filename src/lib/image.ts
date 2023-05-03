6014bd0b7b77a46263d4f02e1b8c139e624c6b2c
Rich Harris <git@rich-harris.dev>
2023-05-03 17:23:49 -0400

    try this

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -4,6 +4,8 @@ export function optimize(src: string, widths = [640, 960, 1280], quality = 75) {
 	if (dev) return src;
 
 	return widths
+		.slice()
+		.sort((a, b) => b - a)
 		.map((width, i) => {
 			const url = `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
 			const descriptor = i < widths.length - 1 ? ` ${width}w` : '';
