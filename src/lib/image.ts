3db4018af767525bb038f5ddc452dae48c78e692
Rich Harris <git@rich-harris.dev>
2023-05-03 16:27:38 -0400

    1080

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
 
-export function optimize(url: string, width = 1000) {
+export function optimize(url: string, width = 1080) {
 	return dev ? url : `/_vercel/image?url=${encodeURIComponent(url)}&w=${width}`;
 }
