36e0778fe7d9f9b50f3e5c17ddaf99e99839233c
Rich Harris <git@rich-harris.dev>
2023-05-03 17:09:11 -0400

    omit final width descriptor

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -4,8 +4,10 @@ export function optimize(url: string, widths = [640, 960, 1280], quality = 75) {
 	if (dev) return url;
 
 	return widths
-		.map((width) => {
-			return `/_vercel/image?url=${encodeURIComponent(url)}&w=${width}&q=${quality} ${width}w`;
+		.map((width, i) => {
+			const url = `/_vercel/image?url=${encodeURIComponent(url)}&w=${width}&q=${quality}`;
+			const descriptor = i < widths.length - 1 ? ` ${width}w` : '';
+			return url + descriptor;
 		})
 		.join(', ');
 }
