5231a3b80c3209a40f6088de344b8bae37c10c6a
Rich Harris <git@rich-harris.dev>
2023-05-05 09:13:13 -0400

    nicer button

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { trapfocus } from '$lib/actions';
 	import { createEventDispatcher } from 'svelte';
-	import { scale } from 'svelte/transition';
+	import { fade, scale } from 'svelte/transition';
 
 	const dispatch = createEventDispatcher();
 </script>
@@ -16,14 +16,19 @@
 
 <!-- svelte-ignore a11y-click-events-have-key-events-->
 <div
-	class="fixed w-screen h-screen bg-[#ffffff88] dark:bg-[#00000088] backdrop-blur-lg backdrop-grayscale-50 top-0 left-0 flex justify-center items-center z-30"
+	class="fixed w-screen h-screen bg-[#ffffff88] dark:bg-[#00000088] backdrop-blur-lg backdrop-grayscale-50 top-0 left-0 z-30"
 	use:trapfocus
-	transition:scale={{ start: 0.95, duration: 200 }}
+	transition:fade={{ duration: 200 }}
 	on:click={(e) => {
 		if (e.target === e.currentTarget) {
 			dispatch('close');
 		}
 	}}
 >
-	<slot />
+	<div
+		class="fixed w-screen h-screen flex justify-center items-center"
+		transition:scale={{ start: 0.95, duration: 200 }}
+	>
+		<slot />
+	</div>
 </div>
