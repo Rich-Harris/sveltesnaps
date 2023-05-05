88072ef5fdd746e4bdec1f688129b8aa00bf5767
Rich Harris <git@rich-harris.dev>
2023-05-05 15:24:34 -0400

    DRY out

---
+++ src/lib/components/Publisher.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let disabled: boolean;
+</script>
+
+<div class="relative flex w-full border-b border-zinc-200 focus-within:border-pink-600">
+	<input
+		class="w-full border-b border-zinc-200 resize-none py-2 pr-20 focus-visible:outline-none"
+		name="description"
+		required
+		autocomplete="off"
+		spellcheck="false"
+		placeholder="add a description"
+	/>
+
+	<button
+		{disabled}
+		class="absolute w-16 h-full right-0 transition-opacity text-pink-600 focus-visible:outline-none focus-visible:bg-pink-100 opacity-0"
+	>
+		publish
+	</button>
+</div>
+
+<style>
+	input:valid + button {
+		opacity: 1;
+	}
+</style>
